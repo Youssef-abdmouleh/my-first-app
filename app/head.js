d4263db3218a4b4f590051a18ae7075698790b9e
youssef abdmouleh <youssef.abdmouleh164@gmail.com>
2023-03-11 11:20:54 +0100

    first commit

---
+++ app/head.js
@@ -0,0 +1,15 @@
+export default function Head(){
+    return(
+        <>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>Ecommerce app</title>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+                precedence="default"
+            />
+        </>
+    )
+}
